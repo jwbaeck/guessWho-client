1cf8f4eeae2dabeb7b2356201999c8cc723bc431
jwbaeck <jwbaeck97@gmail.com>
2024-03-11 23:10:58 +0900

    feat: 게임 입장 버튼 작업

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,25 @@
-function Button() {
+import PropTypes from "prop-types";
+import {
+  ENTER_ROOM_BUTTON_STYLE,
+  ENTER_ROOM_BUTTON_STYLE_WHEN_DISABLED,
+} from "../../utils/styleConstants";
+
+function Button({ onClick, disabled }) {
   return (
-    <button>Button</button>
+    <button
+      type="submit"
+      className={`${ENTER_ROOM_BUTTON_STYLE} ${disabled ? ENTER_ROOM_BUTTON_STYLE_WHEN_DISABLED : ""}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      Enter the Room
+    </button>
   );
 }
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool.isRequired,
+};
 
 export default Button;
