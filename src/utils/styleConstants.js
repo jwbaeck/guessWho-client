f66155a8eb8b8859c9b658017e921c56bd224d8a
jwbaeck <jwbaeck97@gmail.com>
2024-03-11 23:10:29 +0900

    feat: style 속성 상수처리 파일 추가

---
+++ src/utils/styleConstants.js
@@ -0,0 +1,11 @@
+export const INPUT_FORM_STYLE =
+  "flex flex-col items-center mb-4 text-sm font-medium text-green-700 dark:text-green-500";
+
+export const NICKNAME_INPUT_STYLE =
+  "w-full mt-1 p-5 border rounded-md text-lg text-center focus:outline-none focus:ring focus:border-green-700 opacity-50";
+
+export const ENTER_ROOM_BUTTON_STYLE =
+  "bg-green-500 text-white p-4 rounded-md border border-green-700 hover:bg-green-600 focus:outline-none focus:ring focus:border-green-700";
+
+export const ENTER_ROOM_BUTTON_STYLE_WHEN_DISABLED =
+  "cursor-not-allowed opacity-50";
