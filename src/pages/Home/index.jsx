e0f8091f1d619c241e663ae5ccbdddc6a2908fff
jwbaeck <jwbaeck97@gmail.com>
2024-03-11 23:20:48 +0900

    feat: pages 내 Home 컴포넌트 작업

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,18 @@
+import Form from "../../components/Form/Form";
+import LiarImage from "../../assets/homepage_image.png";
+
 function Home() {
   return (
-    <div>Home</div>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <img
+        src={LiarImage}
+        alt="HomePage Theme"
+        className="w-full h-full object-cover absolute z-[-1]"
+      />
+      <div className="mt-80">
+        <Form />
+      </div>
+    </div>
   );
 }
 
