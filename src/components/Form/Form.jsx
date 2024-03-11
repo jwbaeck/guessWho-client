bae161c58d8b97b71718159321269a75001676d4
jwbaeck <jwbaeck97@gmail.com>
2024-03-11 23:19:00 +0900

    feat: Form 컴포넌트 작업

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import NicknameInput from "./NicknameInput";
+import ErrorMessage from "../Info/ErrorMessage";
+import Button from "../Button/Button";
+import { INPUT_FORM_STYLE } from "../../utils/styleConstants";
+
 function Form() {
+  const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = e => {
+    setInputValue(e.target.value);
+  };
+
+  const isInputValid = () => inputValue.length >= 2;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (isInputValid()) {
+      navigate("/lobby");
+    }
+  };
+
   return (
-    <form>form</form>
+    <form className={INPUT_FORM_STYLE} onSubmit={handleSubmit}>
+      <NicknameInput value={inputValue} onChange={handleChange} />
+      <ErrorMessage show={!isInputValid()} />
+      <Button onClick={handleSubmit} disabled={!isInputValid()} />
+    </form>
   );
 }
 
