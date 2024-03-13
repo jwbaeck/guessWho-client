36abf09f88641e51bb4359c5b97c206140ac6f0c
jwbaeck <jwbaeck97@gmail.com>
2024-03-13 17:23:07 +0900

    feat: Table 내 실시간 사용자 입장 관리

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,16 +1,34 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import setUpSocket from "../../services/socketService";
+import useLobbyStore from "../../stores/useLobbyStore";
+import MAX_USERS from "../../utils/constants";
 
 function Table() {
-  const [users, setUsers] = useState([
-    { id: 1, name: "User 1" },
-    { id: 2, name: "User 2" },
-    { id: 3, name: "User 3" },
-    { id: 4, name: "User 4" },
-  ]);
+  const { users, setUsers } = useLobbyStore();
+
+  useEffect(() => {
+    const socketService = setUpSocket();
+
+    const handleUpdateUsers = updatedUserList => {
+      setUsers(updatedUserList);
+    };
+
+    socketService.onUpdateUsers(handleUpdateUsers);
+
+    return () => {
+      socketService.removeAllListeners();
+    };
+  }, []);
+
+  const displayedUsers = [...users];
+
+  for (let i = users.length; i < MAX_USERS; i += 1) {
+    displayedUsers.push({ id: `empty-${i}`, name: "", status: "🔴" });
+  }
 
   return (
-    <table className="text-center w-96 h-80 bg-background-100 opacity-60">
-      <caption className="text-3xl text-900 font-bold mb-4">
+    <table className="text-center font-weight-bold w-96 h-80 bg-background-100 opacity-60">
+      <caption className="text-3xl text-text-50 font-bold mb-4">
         Participants List
       </caption>
       <thead>
@@ -20,10 +38,10 @@ function Table() {
         </tr>
       </thead>
       <tbody>
-        {users.map(user => (
-          <tr key={user.id}>
-            <td className="p-2">{user.name}</td>
-            <td className="p-2">🟢</td>
+        {displayedUsers.map(user => (
+          <tr key={user.id} className="font-bold">
+            <td className="p-2">{user.name || "대기 중..."}</td>
+            <td className="p-2">{user.status || "🟢"}</td>
           </tr>
         ))}
       </tbody>
