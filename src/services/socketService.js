d46254c491c0e201189c3d7bb1dcc974bbaabc72
jwbaeck <jwbaeck97@gmail.com>
2024-03-13 17:24:24 +0900

    feat: 소켓 통신 로직 추가 설정

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -1,7 +1,6 @@
 import { io } from "socket.io-client";
 
 const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_SERVER;
-
 const socket = io(SOCKET_SERVER_URL, {
   cors: {
     origin: "*",
@@ -9,8 +8,10 @@ const socket = io(SOCKET_SERVER_URL, {
 });
 
 const setUpSocket = () => {
-  const submitNickname = nickname => {
-    socket.emit("submitNickname", { nickname });
+  console.log("서버와 소켓 연결 시작.");
+
+  const submitNickname = (nickname, roomNumber) => {
+    socket.emit("submitNickname", { nickname, roomNumber });
   };
 
   const onUpdateUsers = callback => {
@@ -19,9 +20,26 @@ const setUpSocket = () => {
     });
   };
 
+  const joinRoom = roomNumber => {
+    socket.emit("joinRoom", roomNumber);
+  };
+
+  const onMaxCapacityReached = callback => {
+    socket.on("maxCapacityReached", userList => {
+      callback(userList);
+    });
+  };
+
+  const removeAllListeners = () => {
+    socket.removeAllListeners();
+  };
+
   return {
     submitNickname,
     onUpdateUsers,
+    joinRoom,
+    onMaxCapacityReached,
+    removeAllListeners,
   };
 };
 
