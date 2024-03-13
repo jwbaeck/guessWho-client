32a348ec183c6535d7f72ff365eae7a4c33b41c2
jwbaeck <jwbaeck97@gmail.com>
2024-03-13 17:22:32 +0900

    feat: Lobby 페이지 내 전역상태관리 설정

--- src/pages/Lobby/index.jsx
+++ src/pages/Lobby/index.jsx
@@ -1,8 +1,12 @@
 import Table from "../../components/Table";
-import LobbyTheme from "../../assets/lobby_theme.png";
 import LobbyButton from "../../components/Button/LobbyButton";
+import useLobbyStore from "../../stores/useLobbyStore";
+import LobbyTheme from "../../assets/lobby_theme.png";
 
 function Lobby() {
+  const users = useLobbyStore(state => state.users);
+  const usersCount = users.length;
+
   return (
     <div className="flex items-center justify-center min-h-screen relative">
       <img
@@ -11,9 +15,9 @@ function Lobby() {
         className="w-full h-full object-cover absolute z-[-1]"
       />
       <div className="flex flex-col items-center justify-left w-full h-full">
-        <Table />
+        <Table users={users} />
         <div className="mt-10">
-          <LobbyButton />
+          <LobbyButton usersCount={usersCount} />
         </div>
       </div>
     </div>
