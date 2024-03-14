5bc13418f932736ce15f75a1ad9bb4cf4f84445d
jwbaeck <jwbaeck97@gmail.com>
2024-03-14 17:06:33 +0900

    feat: Mission Room 페이지 작업

--- src/pages/Mission/index.jsx
+++ src/pages/Mission/index.jsx
@@ -1,6 +1,36 @@
+import { useState } from "react";
+import Description from "../../components/Info/Description";
+import MissionButton from "../../components/Button/MissionButton";
+import ChatEntranceButton from "../../components/Button/ChatEntranceButton";
+import Modal from "../../components/Modal";
+import MissionRoomTheme from "../../assets/mission_room_theme.png";
+import { THEME_IMAGE_STYLE } from "../../utils/styleConstants";
+
 function MissionRoom() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMissionButtonClicked, setIsMissionButtonClicked] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+    setIsMissionButtonClicked(true);
+  };
+
+  const handleCloseModal = () => setIsModalOpen(false);
+
   return (
-    <div>Mission</div>
+    <div className="flex items-center justify-center min-h-screen relative">
+      <img
+        className={THEME_IMAGE_STYLE}
+        src={MissionRoomTheme}
+        alt="Mission Room Theme"
+      />
+      <div className="flex flex-col items-center">
+        <Description />
+        <MissionButton onClick={handleOpenModal} />
+        <Modal isOpen={isModalOpen} onClose={handleCloseModal} />
+        <ChatEntranceButton isEnabled={isMissionButtonClicked} />
+      </div>
+    </div>
   );
 }
 
