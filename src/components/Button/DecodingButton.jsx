b515d6db9cadb76e75f606b1dc3c3ebb92464a0f
jwbaeck <jwbaeck97@gmail.com>
2024-03-14 17:05:23 +0900

    feat: DecodingButton 생성

---
+++ src/components/Button/DecodingButton.jsx
@@ -0,0 +1,11 @@
+import { FaKey } from "react-icons/fa";
+
+function DecodingButton() {
+  return (
+    <button aria-label="Decoding Button">
+      <FaKey size={30} className="mt-10" />
+    </button>
+  );
+}
+
+export default DecodingButton;
