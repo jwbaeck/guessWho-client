81dbcea19467b0b24e35f8803154a93a356baf78
jwbaeck <jwbaeck97@gmail.com>
2024-03-14 20:28:19 +0900

    feat: 라이어 여부 상태관리 속성 추가

--- src/stores/useLobbyStore.js
+++ src/stores/useLobbyStore.js
@@ -2,7 +2,9 @@ import { create } from "zustand";
 
 const useLobbyStore = create(set => ({
   users: [],
+  isLiar: null,
   setUsers: users => set(() => ({ users })),
+  setIsLiar: isLiar => set({ isLiar }),
   addUser: user => set(state => ({ users: [...state.users, user] })),
   usersCount: () => set(state => state.users.length),
 }));
