d48b8ce9ad6007f8a9681d82a0bd30bfecede152
jwbaeck <jwbaeck97@gmail.com>
2024-03-18 22:54:42 +0900

    feat: Camera 컴포넌트 통한 웹캠 생성 작업

---
+++ src/pages/Chat/Camera.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+import {
+  CAMERA_AREA_STYLE,
+  VIDEO_STYLE,
+  USER_NAME_AREA_STYLE,
+} from "../../utils/styleConstants";
+
+function Camera({ nickname, stream }) {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.srcObject = stream;
+    }
+  }, [stream]);
+
+  return (
+    <div className={CAMERA_AREA_STYLE}>
+      <video ref={videoRef} autoPlay playsInline className={VIDEO_STYLE}>
+        <track kind="captions" />
+      </video>
+      <div className={USER_NAME_AREA_STYLE}>{nickname}</div>
+    </div>
+  );
+}
+
+Camera.defaultProps = {
+  stream: null,
+};
+
+Camera.propTypes = {
+  nickname: PropTypes.string.isRequired,
+  stream: PropTypes.shape({
+    getTracks: PropTypes.func,
+  }),
+};
+
+export default Camera;
