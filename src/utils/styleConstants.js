a969f59d386b2f8794c3f47d4f1c6d7200a82c3c
jwbaeck <jwbaeck97@gmail.com>
2024-03-18 23:02:10 +0900

    chore: 스타일 속성 상수 관리

--- src/utils/styleConstants.js
+++ src/utils/styleConstants.js
@@ -23,3 +23,23 @@ export const CHAT_ENTRANCE_BUTTON_STYLE =
   "text-xl text-text-500 opacity-100 border-2 px-4 py-2 rounded mt-5";
 
 export const THEME_IMAGE_STYLE = "w-full h-full object-cover absolute z-[-1]";
+
+export const CAMERA_GRID_STYLE = {
+  display: "grid",
+  gridTemplateColumns: "repeat(2, 1fr)",
+  gap: "50px",
+  padding: "10px",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-40%, -50%)",
+  width: "90%",
+  height: "auto",
+};
+
+export const CAMERA_AREA_STYLE = "relative w-3/5 text-center";
+
+export const VIDEO_STYLE = "w-full h-auto";
+
+export const USER_NAME_AREA_STYLE =
+  "absolute bottom-0 w-full bg-black bg-opacity-50 text-white text-xs p-1";
