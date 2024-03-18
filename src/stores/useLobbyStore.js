285920ea024dc06e1da30bf422b6833382d06bce
jwbaeck <jwbaeck97@gmail.com>
2024-03-18 23:00:58 +0900

    feat: 클라이언트 미디어스트림 전역상태 관리

--- src/stores/useLobbyStore.js
+++ src/stores/useLobbyStore.js
@@ -6,6 +6,12 @@ const useLobbyStore = create(set => ({
   setUsers: users => set(() => ({ users })),
   setIsLiar: isLiar => set({ isLiar }),
   addUser: user => set(state => ({ users: [...state.users, user] })),
+  updateUserStream: (userId, stream) =>
+    set(state => ({
+      users: state.users.map(user =>
+        user.id === userId ? { ...user, stream } : user,
+      ),
+    })),
   usersCount: () => set(state => state.users.length),
 }));
 
