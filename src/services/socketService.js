cc0dcef1f43d781ef8b0e08d1123fe2e1ef567aa
jwbaeck <jwbaeck97@gmail.com>
2024-03-18 23:01:22 +0900

    feat: webRTC 통신환경 구축

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -1,14 +1,20 @@
 import { io } from "socket.io-client";
 
 const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_SERVER;
-const socket = io(SOCKET_SERVER_URL, {
-  cors: {
-    origin: "*",
-  },
-});
+let socketInstance = null;
 
 const setUpSocket = () => {
+  if (socketInstance) {
+    return socketInstance;
+  }
+
   console.log("서버와 소켓 연결 시작.");
+
+  const socket = io(SOCKET_SERVER_URL, {
+    cors: {
+      origin: "*",
+    },
+  });
 
   const submitNickname = (nickname, roomNumber) => {
     socket.emit("submitNickname", { nickname, roomNumber });
@@ -38,14 +44,52 @@ const setUpSocket = () => {
     socket.removeAllListeners();
   };
 
-  return {
+  const onWebRTCOffer = callback => {
+    socket.on("webRTC-offer", data => {
+      callback(data.sender, data.sdp);
+    });
+  };
+
+  const onWebRTCAnswer = callback => {
+    socket.on("webRTC-answer", data => {
+      callback(data.sender, data.sdp);
+    });
+  };
+
+  const onWebRTCIceCandidate = callback => {
+    socket.on("webRTC-candidate", data => {
+      callback(data.sender, data.candidate);
+    });
+  };
+
+  const sendWebRTCOffer = (target, description) => {
+    socket.emit("webRTC-offer", { target, sdp: description });
+  };
+
+  const sendWebRTCAnswer = (target, description) => {
+    socket.emit("webRTC-answer", { target, sdp: description });
+  };
+
+  const sendWebRTCIceCandidate = (target, candidate) => {
+    socket.emit("webRTC-candidate", { target, candidate });
+  };
+
+  socketInstance = {
     submitNickname,
     onUpdateUsers,
     joinRoom,
     onMaxCapacityReached,
     onRoleInfo,
     removeAllListeners,
+    onWebRTCOffer,
+    onWebRTCAnswer,
+    onWebRTCIceCandidate,
+    sendWebRTCOffer,
+    sendWebRTCAnswer,
+    sendWebRTCIceCandidate,
   };
+
+  return socketInstance;
 };
 
 export default setUpSocket;
