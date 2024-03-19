f1e6ad9d6b5eb133ea932e53b3114936c1911d8c
jwbaeck <jwbaeck97@gmail.com>
2024-03-19 16:32:09 +0900

    feat: 클라이언트 실시간 입장확인 및 webRTC 환경설정  작업

--- src/services/socketService.js
+++ src/services/socketService.js
@@ -4,90 +4,91 @@ const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_SERVER;
 let socketInstance = null;
 
 const setUpSocket = () => {
-  if (socketInstance) {
-    return socketInstance;
+  if (!socketInstance) {
+    console.log("서버와 소켓 연결 시작.");
+
+    const socket = io(SOCKET_SERVER_URL, {
+      cors: {
+        origin: "*",
+      },
+    });
+
+    const submitNickname = (nickname, roomNumber) => {
+      socket.emit("submitNickname", { nickname, roomNumber });
+    };
+
+    const onUpdateUsers = callback => {
+      socket.on("updateUsers", callback);
+    };
+
+    const joinRoom = roomNumber => {
+      socket.emit("joinRoom", roomNumber);
+    };
+
+    const onMaxCapacityReached = callback => {
+      socket.on("maxCapacityReached", callback);
+    };
+
+    const onRoleInfo = callback => {
+      socket.on("roleInfo", callback);
+    };
+
+    const removeAllListeners = () => {
+      socket.removeAllListeners();
+    };
+
+    const onUserEntered = callback => {
+      socket.on("userEntered", callback);
+    };
+
+    const enterChatRoom = () => {
+      socket.emit("userEnteredChatRoom");
+    };
+
+    const onWebRTCOffer = callback => {
+      socket.on("webRTC-offer", ({ sender, sdp }) => callback(sender, sdp));
+    };
+
+    const onWebRTCAnswer = callback => {
+      socket.on("webRTC-answer", ({ sender, sdp }) => callback(sender, sdp));
+    };
+
+    const onWebRTCIceCandidate = callback => {
+      socket.on("webRTC-candidate", ({ sender, candidate }) =>
+        callback(sender, candidate),
+      );
+    };
+
+    const sendWebRTCOffer = (target, description) => {
+      socket.emit("webRTC-offer", { target, sdp: description });
+    };
+
+    const sendWebRTCAnswer = (target, description) => {
+      socket.emit("webRTC-answer", { target, sdp: description });
+    };
+
+    const sendWebRTCIceCandidate = (target, candidate) => {
+      socket.emit("webRTC-candidate", { target, candidate });
+    };
+
+    socketInstance = {
+      socket,
+      submitNickname,
+      onUpdateUsers,
+      joinRoom,
+      onMaxCapacityReached,
+      onRoleInfo,
+      removeAllListeners,
+      onUserEntered,
+      enterChatRoom,
+      onWebRTCOffer,
+      onWebRTCAnswer,
+      onWebRTCIceCandidate,
+      sendWebRTCOffer,
+      sendWebRTCAnswer,
+      sendWebRTCIceCandidate,
+    };
   }
-
-  console.log("서버와 소켓 연결 시작.");
-
-  const socket = io(SOCKET_SERVER_URL, {
-    cors: {
-      origin: "*",
-    },
-  });
-
-  const submitNickname = (nickname, roomNumber) => {
-    socket.emit("submitNickname", { nickname, roomNumber });
-  };
-
-  const onUpdateUsers = callback => {
-    socket.on("updateUsers", userList => {
-      callback(userList);
-    });
-  };
-
-  const joinRoom = roomNumber => {
-    socket.emit("joinRoom", roomNumber);
-  };
-
-  const onMaxCapacityReached = callback => {
-    socket.on("maxCapacityReached", userList => {
-      callback(userList);
-    });
-  };
-
-  const onRoleInfo = callback => {
-    socket.on("roleInfo", callback);
-  };
-
-  const removeAllListeners = () => {
-    socket.removeAllListeners();
-  };
-
-  const onWebRTCOffer = callback => {
-    socket.on("webRTC-offer", data => {
-      callback(data.sender, data.sdp);
-    });
-  };
-
-  const onWebRTCAnswer = callback => {
-    socket.on("webRTC-answer", data => {
-      callback(data.sender, data.sdp);
-    });
-  };
-
-  const onWebRTCIceCandidate = callback => {
-    socket.on("webRTC-candidate", data => {
-      callback(data.sender, data.candidate);
-    });
-  };
-
-  const sendWebRTCOffer = (target, description) => {
-    socket.emit("webRTC-offer", { target, sdp: description });
-  };
-
-  const sendWebRTCAnswer = (target, description) => {
-    socket.emit("webRTC-answer", { target, sdp: description });
-  };
-
-  const sendWebRTCIceCandidate = (target, candidate) => {
-    socket.emit("webRTC-candidate", { target, candidate });
-  };
-
-  socketInstance = {
-    submitNickname,
-    onUpdateUsers,
-    joinRoom,
-    onMaxCapacityReached,
-    onRoleInfo,
-    removeAllListeners,
-    onWebRTCOffer,
-    onWebRTCAnswer,
-    onWebRTCIceCandidate,
-    sendWebRTCOffer,
-    sendWebRTCAnswer,
-    sendWebRTCIceCandidate,
-  };
 
   return socketInstance;
 };
