470803c4383740de8da4f6de63fcabc99849a8cc
jwbaeck <jwbaeck97@gmail.com>
2024-03-19 16:42:19 +0900

    feat: 입장상태 표시 역할의 EntranceMessage 컴포넌트 생성

---
+++ src/pages/Chat/EntranceMessage.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { IoEnter } from "react-icons/io5";
+import { ENTRANCE_MESSAGE_STYLE } from "../../utils/styleConstants";
+
+function EntranceMessage({ userName, onHide }) {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [onHide]);
+
+  if (!isVisible) {
+    return null;
+  }
+
+  return (
+    <div className={ENTRANCE_MESSAGE_STYLE}>
+      <IoEnter className="mr-2" />
+      <span>{userName} 님이 채팅방에 입장하였습니다.</span>
+    </div>
+  );
+}
+
+EntranceMessage.propTypes = {
+  userName: PropTypes.string.isRequired,
+  onHide: PropTypes.func.isRequired,
+};
+
+export default EntranceMessage;
