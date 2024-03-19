0397b3326759ada1ec749e5bb627ea61bee96103
jwbaeck <jwbaeck97@gmail.com>
2024-03-19 16:30:45 +0900

    chore: 스타일 속성 추가

--- src/utils/styleConstants.js
+++ src/utils/styleConstants.js
@@ -1,3 +1,6 @@
+export const PAGE_STYLE =
+  "flex items-center justify-center min-h-screen relative";
+
 export const INPUT_FORM_STYLE =
   "flex flex-col items-center text-sm font-medium mb-4";
 
@@ -24,22 +27,15 @@ export const CHAT_ENTRANCE_BUTTON_STYLE =
 
 export const THEME_IMAGE_STYLE = "w-full h-full object-cover absolute z-[-1]";
 
-export const CAMERA_GRID_STYLE = {
-  display: "grid",
-  gridTemplateColumns: "repeat(2, 1fr)",
-  gap: "50px",
-  padding: "10px",
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-40%, -50%)",
-  width: "90%",
-  height: "auto",
-};
+export const CAMERA_GRID_STYLE =
+  "grid grid-cols-2 gap-5 max-w-screen-lg mx-auto";
 
 export const CAMERA_AREA_STYLE = "relative w-3/5 text-center";
 
 export const VIDEO_STYLE = "w-full h-auto";
 
 export const USER_NAME_AREA_STYLE =
-  "absolute bottom-0 w-full bg-black bg-opacity-50 text-white text-xs p-1";
+  "absolute bottom-0 w-full bg-black bg-opacity-50 text-white text-lg p-1";
+
+export const ENTRANCE_MESSAGE_STYLE =
+  "fixed top-0 left-1/2 transform -translate-x-1/2 bg-blue-500 text-white p-2 rounded-md mt-4 flex items-center";
