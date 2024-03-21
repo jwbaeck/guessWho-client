7239b407c5403dbd48915d32f7e3f64bbf712b83
jwbaeck <jwbaeck97@gmail.com>
2024-03-21 21:00:35 +0900

    feat: 클릭여부에 따른 VoteButton 디자인 작업

--- src/pages/Chat/VoteButton.jsx
+++ src/pages/Chat/VoteButton.jsx
@@ -1,27 +1,32 @@
-import { FaVoteYea } from "react-icons/fa";
+import { FaVoteYea, FaCheck } from "react-icons/fa";
 import PropTypes from "prop-types";
 import { GO_TO_VOTE_STYLE } from "../../utils/styleConstants";
 
-function VoteButton({ isActive, onClick }) {
+function VoteButton({ onClick, isActive, isVoted }) {
   return (
     <button
       className={`${GO_TO_VOTE_STYLE} ${!isActive ? "opacity-50 cursor-not-allowed" : ""}`}
       disabled={!isActive}
       onClick={onClick}
     >
-      <FaVoteYea className="inline mr-2" size={50} />
-      투표하기
+      {isVoted ? (
+        <FaCheck className="inline mr-2" size={50} />
+      ) : (
+        <FaVoteYea className="inline mr-2" size={50} />
+      )}
     </button>
   );
 }
 
 VoteButton.defaultProps = {
   onClick: () => {},
+  isVoted: false,
 };
 
 VoteButton.propTypes = {
-  isActive: PropTypes.bool.isRequired,
   onClick: PropTypes.func,
+  isActive: PropTypes.bool.isRequired,
+  isVoted: PropTypes.bool,
 };
 
 export default VoteButton;
