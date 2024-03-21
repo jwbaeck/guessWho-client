6cd609cbb0b090840fd40af37a5334835c24ed0b
jwbaeck <jwbaeck97@gmail.com>
2024-03-21 11:20:16 +0900

    chore: 스타일 속성 추가

--- src/utils/styleConstants.js
+++ src/utils/styleConstants.js
@@ -58,3 +58,9 @@ export const VOTE_TABLE_STYLE =
 
 export const CHECK_BOX_STYLE =
   "w-6 h-6 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600";
+
+export const RESULT_BUTTON_STYLE =
+  "text-xl text-warn-600 font-bold border-4 opacity-100 px-4 py-2 rounded border-blue-500";
+
+export const RESULT_BUTTON_STYLE_WHEN_DISABLED =
+  "text-xl text-gray-500 font-bold border-4 opacity-100 px-4 py-2 rounded border-gray-500";
