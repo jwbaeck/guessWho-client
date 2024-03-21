54218dfb58b56da629863954662845edf2ca056b
jwbaeck <jwbaeck97@gmail.com>
2024-03-21 11:26:42 +0900

    feat: VotingForm 내 서버와의 실시간 투표 로직 처리

--- src/pages/Vote/VotingForm.jsx
+++ src/pages/Vote/VotingForm.jsx
@@ -1,17 +1,44 @@
+import { useState, useEffect } from "react";
 import VoteDescription from "./VoteDescription";
 import VoteTable from "./VoteTable";
 import VotingButton from "./VotingButton";
+import ResultButton from "./ResultButton";
+import setUpSocket from "../../services/socketService";
 
 function VotingForm() {
+  const [selectedUserId, setSelectedUserId] = useState(null);
+  const [resultAvailable, setResultAvailable] = useState(false);
+  const socket = setUpSocket();
+
+  useEffect(() => {
+    socket.onVoteResults(data => {
+      setResultAvailable(true);
+    });
+
+    return () => {
+      socket.onVoteResults(null);
+    };
+  }, [socket]);
+
+  const submitVote = () => {
+    if (selectedUserId) {
+      socket.submitVote({ votedForId: selectedUserId });
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <VoteDescription />
       <div className="w-full flex justify-center">
-        <VoteTable />
+        <VoteTable
+          selectedUserId={selectedUserId}
+          setSelectedUserId={setSelectedUserId}
+        />
       </div>
       <div className="w-full flex justify-center mt-5">
-        <VotingButton />
+        <VotingButton onVoteSubmit={submitVote} disabled={!selectedUserId} />
       </div>
+      <ResultButton disabled={!resultAvailable} />
     </div>
   );
 }
