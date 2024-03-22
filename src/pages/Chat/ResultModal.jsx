1b19ffea004b7c42820056d13e5c3c172291f265
jwbaeck <jwbaeck97@gmail.com>
2024-03-22 21:38:53 +0900

    feat: ResultModal 내 전역 상태관리 활용한 게임 결과 description 작업

--- src/pages/Chat/ResultModal.jsx
+++ src/pages/Chat/ResultModal.jsx
@@ -1,35 +1,36 @@
 import PropTypes from "prop-types";
+import { FaVoteYea } from "react-icons/fa";
+import { IoIosCloseCircle } from "react-icons/io";
+import useLobbyStore from "../../stores/useLobbyStore";
 import useGameResultStore from "../../stores/useGameResultStore";
+import { describeGameResult } from "../../utils/gameUtils";
+import { MODAL_STYLE } from "../../utils/styleConstants";
 
 function ResultModal({ onClose }) {
-  const { isLiarCorrectlyIdentified, votes, topVotedUserId } =
-    useGameResultStore();
+  const lobbyStore = useLobbyStore();
+  const gameResultStore = useGameResultStore();
+
+  const gameResultMessage = describeGameResult(
+    lobbyStore.users,
+    gameResultStore.isLiarCorrectlyIdentified,
+    gameResultStore.votes,
+  );
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white p-5 rounded-lg shadow-xl">
-        <h2 className="text-xl font-bold mb-4">투표 결과</h2>
-        {isLiarCorrectlyIdentified ? (
-          <p>거짓말쟁이가 정확히 식별되었습니다!</p>
-        ) : (
-          <p>거짓말쟁이를 찾는 데 실패했습니다.</p>
-        )}
-        <div className="mt-4">
-          <p>가장 많은 표를 받은 사용자 ID: {topVotedUserId}</p>
-          <p>투표 상세:</p>
-          <ul>
-            {Object.entries(votes).map(([userId, voteCount]) => (
-              <li key={userId}>
-                {userId}: {voteCount}표
-              </li>
-            ))}
-          </ul>
-        </div>
+    <div className={`${MODAL_STYLE} opacity-90`}>
+      <div className="bg-background-100 text-center p-4 rounded-lg shadow-xl w-2/4 max-w-4xl">
+        <h2 className="text-3xl font-bold mb-4 text-warn-500">
+          <FaVoteYea className="inline mr-2 mb-2" />
+          투표 결과
+          <FaVoteYea className="inline ml-2 mb-2" />
+        </h2>
+        <pre className="text-xl font-bold">{gameResultMessage}</pre>
         <button
           onClick={onClose}
           className="mt-4 px-4 py-2 bg-blue-500 text-black rounded hover:bg-blue-700"
+          aria-label="Close"
         >
-          닫기
+          <IoIosCloseCircle size={40} className="mt-10" />
         </button>
       </div>
     </div>
