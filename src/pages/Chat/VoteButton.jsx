44e7056544cffadd5949b38caf3421c99dbb8f89
jwbaeck <jwbaeck97@gmail.com>
2024-03-22 16:33:30 +0900

    feat: 게임시간 종료에 따른 VoteButton 가시 속성 작업

--- src/pages/Chat/VoteButton.jsx
+++ src/pages/Chat/VoteButton.jsx
@@ -2,13 +2,11 @@ import { FaVoteYea, FaCheck } from "react-icons/fa";
 import PropTypes from "prop-types";
 import { GO_TO_VOTE_STYLE } from "../../utils/styleConstants";
 
-function VoteButton({ onClick, isActive, isVoted }) {
+function VoteButton({ onClick, isTimeUp, isVoted }) {
+  const VISIBLE = isTimeUp ? "visible" : "invisible";
+
   return (
-    <button
-      className={`${GO_TO_VOTE_STYLE} ${!isActive ? "opacity-50 cursor-not-allowed" : ""}`}
-      disabled={!isActive}
-      onClick={onClick}
-    >
+    <button className={`${GO_TO_VOTE_STYLE} ${VISIBLE}`} onClick={onClick}>
       {isVoted ? (
         <FaCheck className="inline mr-2" size={50} />
       ) : (
@@ -25,8 +23,8 @@ VoteButton.defaultProps = {
 
 VoteButton.propTypes = {
   onClick: PropTypes.func,
-  isActive: PropTypes.bool.isRequired,
   isVoted: PropTypes.bool,
+  isTimeUp: PropTypes.bool.isRequired,
 };
 
 export default VoteButton;
