6bd7f51bbcd9c57ce037ee2e875f3e91c9cda30b
jwbaeck <jwbaeck97@gmail.com>
2024-03-22 16:33:04 +0900

    chore: 웹카메라 및 투표 관련 컴포넌트 스타일 속성 작업

--- src/utils/styleConstants.js
+++ src/utils/styleConstants.js
@@ -27,21 +27,16 @@ export const CHAT_ENTRANCE_BUTTON_STYLE =
 
 export const THEME_IMAGE_STYLE = "w-full h-full object-cover absolute z-[-1]";
 
-export const CAMERA_GRID_STYLE =
-  "grid grid-cols-2 gap-5 max-w-screen-lg mx-auto";
+export const CAMERA_GRID_STYLE = "grid grid-cols-2 gap-5 auto-rows-[20rem]";
 
-export const CAMERA_GRID_TRANSFORM_STYLE = {
-  transform: "translateX(10%) translateY(-18%)",
-};
-
-export const CAMERA_AREA_STYLE = "relative w-3/5 text-center";
+export const CAMERA_AREA_STYLE = "relative text-center";
 
 export const SELECTED_CAMERA_STYLE = "border-4 border-warn-500";
 
-export const VIDEO_STYLE = "w-full h-auto";
+export const VIDEO_STYLE = "w-full h-full";
 
 export const USER_NAME_AREA_STYLE =
-  "absolute bottom-0 w-full bg-black bg-opacity-50 text-white text-lg p-1";
+  "absolute bottom-0 w-full bg-black bg-opacity-50 text-text-100 text-lg p-1";
 
 export const ENTRANCE_MESSAGE_STYLE =
   "bg-blue-500 text-white p-2 rounded-md mb-2 flex items-center";
@@ -53,7 +48,7 @@ export const GAME_TIMER_STYLE =
   "fixed right-5 top-5 flex items-center space-x-2 text-white text-2xl";
 
 export const GO_TO_VOTE_STYLE =
-  "absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-blue-500 text-white text-xl font-bold py-2 px-4 rounded mb-10";
+  "bg-blue-500 text-white text-xl font-bold py-2 px-4 rounded mt-10";
 
 export const VOTE_TABLE_STYLE =
   "lg:w-2/5 xl:w-1/3 mx-auto mt-10 text-left bg-black opacity-50 border-solid";
@@ -62,7 +57,7 @@ export const CHECK_BOX_STYLE =
   "w-6 h-6 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600";
 
 export const RESULT_BUTTON_STYLE =
-  "text-xl text-warn-600 font-bold border-4 opacity-100 px-4 py-2 rounded border-blue-500";
+  "text-xl text-text-500 font-bold border-4 opacity-100 px-5 py-2 rounded border-green-700 mt-5";
 
 export const RESULT_BUTTON_STYLE_WHEN_DISABLED =
-  "text-xl text-gray-500 font-bold border-4 opacity-100 px-4 py-2 rounded border-gray-500";
+  "text-xl text-gray-500 font-bold border-4 opacity-100 px-4 py-2 rounded border-gray-500 mt-10";
